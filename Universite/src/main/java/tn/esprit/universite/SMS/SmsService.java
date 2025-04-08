3839e7e474979a8d42a67493c6674204776ffc8c
Guesmi Melek <melek.guesmi@esprit.tn>
2025-04-08 10:48:11 +0000

    Basic Crud

--- Universite/src/main/java/tn/esprit/universite/SMS/SmsService.java
+++ Universite/src/main/java/tn/esprit/universite/SMS/SmsService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class SmsService {
 
-
+    private final String accountSid = "AC35bab9fceb750f598bc18a7f4c3f6e2c"; // Replace with your actual SID
+    private final String authToken = "7b3d08efc8fcb74094d0bbf273675e74"; // Replace with your actual Auth Token
+    private final String fromNumber = "++1 859 694 9707"; // Replace with your Twilio phone number
 
     public SmsService() {
         Twilio.init(accountSid, authToken);
